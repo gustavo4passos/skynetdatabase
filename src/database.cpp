733cf381c6f589ffc705480fe771429061b82288
Gustavo Passos <gustavodiscorda@hotmail.com>
2019-05-23 03:52:43 -0300

    Implements page spliting

--- src/database.cpp
+++ src/database.cpp
@@ -1,4 +1,5 @@
 #include "database.h"
+#include <iostream>
 #include <cstring>
 
 Database::Database()
@@ -21,20 +22,15 @@ Database::Database()
 
 Database::~Database()
 {
-    // Stores header data to file
-    Header h = { N, m_level, m_numberOfPages, m_numberOfEntries, m_next };
-    m_mainDataFile.seekp(std::ios::beg);
-    m_mainDataFile.write((const char*)&h, sizeof(Header));
-
     m_mainDataFile.close();
 }
 
 int Database::InsertEntry(const char key[21], const char value[51])
 {
     unsigned hash = CalcHash(key);
-    unsigned offset = CalcIndexOffset(hash);
+    unsigned indexOffset = CalcIndexOffset(hash);
 
-    m_mainDataFile.seekp(offset, std::ios::beg);
+    m_mainDataFile.seekp(indexOffset, std::ios::beg);
     IndexHeader ih;
     m_mainDataFile.read((char*)&ih, INDEX_HEADER_SIZE);
     std::streampos firstEmptyPos = m_mainDataFile.tellp();
@@ -48,24 +44,33 @@ int Database::InsertEntry(const char key[21], const char value[51])
 
     WriteEntryAtPosition(key, value, firstEmptyPos);
     
-    // Update index header
-    ih.numberOfEntries++;
-    m_mainDataFile.seekp(offset);
-    m_mainDataFile.write((const char*)&ih, INDEX_HEADER_SIZE);
-
     // Update database header (only in memory)
     // The final value is stored only when the program is closed
     m_numberOfEntries++;
+
+    // Test if max limit has been exceeded
+    float load = m_numberOfEntries / ((float)m_numberOfPages * ENTRIES_PER_PAGE);
+    if(load > MAX_LIMIT)
+    {
+        SplitPage(m_next);
+    }
+
+    // Update index header
+    ih.numberOfEntries++;
+    m_mainDataFile.seekp(indexOffset);
+    m_mainDataFile.write((const char*)&ih, INDEX_HEADER_SIZE);
+
+    UpdateMainHeader();
 
     return 0;
 }
 
 int Database::GetEntry(const char key[21], char outValue[51])
 {
-    unsigned index_offset = CalcOffsetFromKey(key);
+    unsigned index_offset = CalcIndexOffsetFromKey(key);
 
     unsigned offset = index_offset + INDEX_HEADER_SIZE;
-    m_mainDataFile.seekp(offset, std::ios::beg);
+    m_mainDataFile.seekg(offset, std::ios::beg);
     while(offset < index_offset + INDEX_SIZE)
     {
         Entry e = { 0, 0, 0, 0};
@@ -113,30 +118,40 @@ void Database::CreateMainDataFile()
     OpenMainDataFile();
 }
 
+void Database::UpdateMainHeader()
+{
+    // Stores header data to file
+    Header h = { N, m_level, m_numberOfPages, m_numberOfEntries, m_next };
+    m_mainDataFile.seekp(std::ios::beg);
+    m_mainDataFile.write((const char*)&h, sizeof(Header));
+}
+
 unsigned Database::CalcHash(const char key[21])
 {
-    return (int)key[0] % (N * PowerOfTwo(m_level));
+    unsigned hash = (unsigned)key[0] % (N * TwoToThePower(m_level));
+    if(hash < m_next) return (unsigned)key[0] % (N * TwoToThePower(m_level + 1));
+    return hash;
 }
 
-unsigned Database::PowerOfTwo(int exponent)
+unsigned Database::TwoToThePower(int exponent)
 {
     if(exponent == 0) return 1;
-    return 2 << exponent;
+    return 2 << (exponent - 1);
 }
 
-unsigned Database::CalcIndexOffset(int index)
+unsigned Database::CalcIndexOffset(int index, bool isMainFile)
 {
-    return HEADER_SIZE + index * INDEX_SIZE;
+    return (isMainFile ? HEADER_SIZE : 0) + index * INDEX_SIZE;
 }
 
-unsigned Database::CalcOffsetFromKey(const char key[21])
+unsigned Database::CalcIndexOffsetFromKey(const char key[21])
 {
     return CalcIndexOffset(CalcHash(key));
 }
 
 std::streampos Database::FindFirstEmptySlot()
 {
-    unsigned startOffset = m_mainDataFile.tellg();
+    unsigned startOffset = m_mainDataFile.tellp();
     while(m_mainDataFile.tellp() < startOffset + INDEX_SIZE)
     {
         char check = '\0';
@@ -145,6 +160,8 @@ std::streampos Database::FindFirstEmptySlot()
         m_mainDataFile.seekp((int)m_mainDataFile.tellp() + ENTRY_SIZE - 1);
     }
 
+
+    if(!m_mainDataFile.good()) std::cout << "Stream is bad!" << std::endl;
     return -1;
 }
 
@@ -156,4 +173,74 @@ void Database::WriteEntryAtPosition(
     m_mainDataFile.seekp(pos);
     m_mainDataFile.write((const char*)key, 21);
     m_mainDataFile.write((const char*)value, 51);
+}
+
+void Database::SplitPage(unsigned page)
+{
+    m_numberOfPages++;
+    // Advances next pointer
+    m_next = (m_next + 1) % (N * (m_level + 1));
+    if(m_next == 0) m_level++;
+
+    // Fill the next page with 0's
+    m_mainDataFile.seekp((m_numberOfPages + 1) * INDEX_SIZE + HEADER_SIZE - 2);
+    m_mainDataFile.write("", 1);
+
+    DistributeEntries(page);
+}
+
+void Database::DistributeEntries(unsigned page)
+{
+    IndexHeader originalIndexHeader;
+    std::fstream* currentDataFile = &m_mainDataFile;
+
+    // Go to first entry in the index
+    m_mainDataFile.seekp(CalcIndexOffset(page));
+    m_mainDataFile.read((char *)&originalIndexHeader, INDEX_HEADER_SIZE);
+    if(originalIndexHeader.numberOfEntries == 0) return;
+
+    for(unsigned i = 0; i < originalIndexHeader.numberOfEntries;)
+    {
+        Entry e = { 0, 0 };
+        currentDataFile->read((char *)&e, ENTRY_SIZE);
+
+        // Check if entry is empty
+        if(e.key[0] != '\0')
+        {
+            // Advance entry index
+            i++;
+
+            unsigned hash = CalcHash(e.key);
+            if(hash != page)
+            {
+                // Returns pointer to beggining of entry
+                currentDataFile->seekp(
+                    (unsigned)currentDataFile->tellp() - ENTRY_SIZE);
+
+                // Delete entry from current index,
+                // i.e. sets the first byte of
+                // its key to \0
+                currentDataFile->write("\0", 1);
+                
+                // Update total number of entries before inserting
+                // entry into split page
+                m_numberOfEntries--;
+
+                // Update index header
+                originalIndexHeader.numberOfEntries--;
+
+                // Puts file pointer back at the end of current entry
+                currentDataFile->seekp(
+                    (unsigned)currentDataFile->tellp() + ENTRY_SIZE - 1);
+
+                // Insert into the new page
+                InsertEntry(e.key, e.value);
+
+                continue;
+            }
+        }
+    }
+    // Write header to file
+    m_mainDataFile.seekp(CalcIndexOffset(page));
+    m_mainDataFile.write((const char*)&originalIndexHeader, INDEX_HEADER_SIZE);
 }
--- src/database.h
+++ src/database.h
@@ -1,6 +1,13 @@
+/*  Universidade Federal da Bahia      */
+/*  Estrutura de Dados e Algoritmos II */
+/*  Alisson Souza                      */
+/*  Gustavo Passos                     */
+/*  Kaio Carvalho                      */
+
 #pragma once
 
 #include <fstream>
+#include <vector>
 
 #pragma pack(push, 1)
 struct Entry 
@@ -25,14 +32,41 @@ public:
 
 private:
     std::fstream m_mainDataFile;
+    std::vector<std::fstream> m_extensionPages;
 
+    // Tries to open the main data file.
+    // Returns true if the file was successfully opened.
+    // Returns false otherwise.
     bool OpenMainDataFile();
-    void CreateHeaderFile();
+
+    // Creates the main data file for the first time
+    // using default values based on the parameters
+    // defined below (N and entries per page). 
+    // The main data file contains the main header
+    // (descbried by the Header sctructure) and each
+    // index first page.
+    // Each index is composed by an index header 
+    // (described the the IndexHeader structure)
+    // and the entries. The ammount of entries
+    // per index is defined by the ENTRIES_PER_PAGE constant
     void CreateMainDataFile();
+
+    // Store the database current state to the main data file
+    void UpdateMainHeader();
+
     unsigned CalcHash(const char key[21]);
-    unsigned PowerOfTwo(int exponent);
-    unsigned CalcIndexOffset(int index);
-    unsigned CalcOffsetFromKey(const char key[21]);
+
+    unsigned TwoToThePower(int exponent);
+
+    // Returns the position in the file of an index
+    unsigned CalcIndexOffset(int index, bool isMainFile = true);
+
+    // Returns the position of the file of the index where
+    // key[21] hashes to
+    unsigned CalcIndexOffsetFromKey(const char key[21]);
+
+    // Finds the file offset which represents the first empty slot 
+    // of an index
     std::streampos FindFirstEmptySlot();
     void WriteEntryAtPosition(
         const char key[21], 
@@ -40,15 +74,18 @@ private:
         const std::streampos& pos
     );
 
+    void SplitPage(unsigned page);
+    void DistributeEntries(unsigned page);
+
     unsigned m_level;
     unsigned m_next;
     unsigned m_numberOfEntries;
     unsigned m_numberOfPages;
     
-    const float MIN = 0.3f;
-    const float MAX = 0.8f;
-    const unsigned N = 11;
-    const unsigned ENTRIES_PER_PAGE = 11;
+    const float MIN_LIMIT = 0.3f;
+    const float MAX_LIMIT = 0.8f;
+    const unsigned N = 3;
+    const unsigned ENTRIES_PER_PAGE = 2;
     const char* MAIN_DATA_FILE_NAME = "data.dat";
     const unsigned HEADER_SIZE = sizeof(Header);
     const unsigned ENTRY_SIZE = sizeof(Entry);
